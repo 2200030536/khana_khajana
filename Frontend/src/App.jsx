3dd7b3cd800606a051761674395ff7995edc0d4f
ROHITKUMARGUPTA <2200030536@kluniversity.in>
2025-04-05 21:34:42 +0530

    route correction

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import StudentSignup from './components/StudentUserSignup';
 import MessUserSignup from './components/MessUserSignup';
 import AdminUser from './pages/Admin User/AdminUser';
-import './App.css';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Menu from './pages/Menu';
 import MessUserProfile from './pages/Mess User/MessUserProfile';
 import MenuManager from './pages/Mess User/MenuManager';
 import MessUserPage from './pages/Mess User/MessUserPage';
+import MealPlans from './pages/Student User/MealsPlans';
+import ProfilePage from './pages/Student User/ProfilePage';
+
+
 
 const App = () => {
   return (
@@ -33,7 +37,8 @@ const App = () => {
         <Route path="/menu-manager" element={<MenuManager />} />
         <Route path="/profile" element={<ProfilePage/>} />
         <Route path="/meals-plans" element={<MealPlans />} />
-        <Route path="/studentdetails" element={< ProfileDetails/>} />
+
+
 
       </Routes>
     </BrowserRouter>
