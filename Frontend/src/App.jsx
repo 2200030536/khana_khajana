b316775ca45202597cf2ff80d08bb2a695913d2d
Rohit Ranjan <rrohit2005@gmail.com>
2025-04-05 09:51:04 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import CreateMenu from './components/CreateMenu';
-// import ViewMenus from './components/ViewMenus';
-// import UpdateMenu from './components/UpdateMenu';
-// import DeleteMenu from './components/DeleteMenu';
-// import MenuCrud from './components/MenuCrud';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import StudentSignup from './components/StudentUserSignup';
 import MessUserSignup from './components/MessUserSignup';
-// import Profile from './components/Profile';
 import MessUser from './pages/Mess User/MessUser';
 import AdminUser from './pages/Admin User/AdminUser';
 import './App.css';
@@ -22,9 +16,6 @@ import MenuManager from './pages/Mess User/MenuManager';
 import ProfilePage from './pages/Student user/ProfilePage';
 import MessUserPage from './pages/Mess User/MessUserPage';
 
-
-
-
 const App = () => {
   return (
     <BrowserRouter>
@@ -34,14 +25,6 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/student-signup" element={<StudentSignup />} />
         <Route path="/messuser-signup" element={<MessUserSignup />} />
-        {/* <Route path="/profile" element={<Profile />} /> */}
-        {/* <Route path="/menu-crud" element={<MenuCrud />} />
-        <Route path="/create-menu" element={<CreateMenu />} />
-        <Route path="/view-menus" element={<ViewMenus />} />
-        <Route path="/update-menu" element={<UpdateMenu />} />
-        <Route path="/delete-menu" element={<DeleteMenu />} /> */}
-        {/* mess user dashboard */}
-        {/* <Route path="/messDashboard" element={<MessUser/>}></Route> */}
         <Route path="/messDashboard" element={<MessUserPage/>}></Route>
         {/* admin dashboard */}
         <Route path="/adminDashboard" element={<AdminUser/>}></Route>
@@ -51,8 +34,6 @@ const App = () => {
         <Route path="/mess-user/profile" element={<MessUserProfile />} />
         <Route path="/menu-manager" element={<MenuManager />} />
         <Route path="/profile" element={<ProfilePage />} />
-
-
 
       </Routes>
     </BrowserRouter>
