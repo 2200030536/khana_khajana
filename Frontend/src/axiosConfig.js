4b2bb39bf2c681524d92c61d5a1ad03441ec7fde
Rohit Kumar Gupta <116941755+2200030536@users.noreply.github.com>
2025-07-28 08:19:46 +0530

    Update axiosConfig.js

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://khana-khajana-i2rp-1omjcbd84-rohits-projects-c8d7edd1.vercel.app//', // for local
+  baseURL: 'https://khana-khajana-i2rp-1omjcbd84-rohits-projects-c8d7edd1.vercel.app', // for local
   withCredentials: true // Allow credentials (cookies) to be sent
 });
 
