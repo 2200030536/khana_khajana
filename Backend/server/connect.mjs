7b7b037c12581ee38dc90eb753037a931435a3b6
Rohit Kumar Gupta <116941755+2200030536@users.noreply.github.com>
2025-07-28 07:53:46 +0530

    Update connect.mjs

--- Backend/server/connect.mjs
+++ Backend/server/connect.mjs
@@ -6,15 +6,14 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect('mongodb+srv://rohitkumarg698:0908@cluster0.qmw92.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     await mongoose.connect(process.env.ATLAS_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
     console.log('Connected to MongoDB');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit process with failure
+    console.error('Error connecting to MongoDB:', error.message);
+    throw new Error('DB connection failed'); // Explicit error
   }
 };
 
