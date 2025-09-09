6e94e844d05ab48c090d865efbf1a2932cc8230f
ROHITKUMARGUPTA <2200030536@kluniversity.in>
2025-09-09 08:20:54 +0530

    deployment

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://khana-khajana-y852.vercel.app/' 
+    ? 'https://khana-khajana-0lwr.onrender.com' 
     : 'http://localhost:3001', // Local development backend
   withCredentials: true // Allow credentials (cookies) to be sent
 });
